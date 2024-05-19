b1a21428efa26bb02094027f2268d056c562746f
Web <web@it.step>
2024-05-19 19:20:54 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Playstation } from "./Playstation";
 import { Eggs } from "./Eggs";
-import {Ground} from './Ground';
+import { Ground } from "./Ground";
+
+// TODO input value two way binding
 
 export const App = () => {
+  const [shouldShowText, setShouldShowText] = useState(false);
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const show = () => {
+    setShouldShowText((prevShowText) => !prevShowText);
+  };
+
   return (
     <>
+      <button onClick={show}>show</button>
+      {shouldShowText && <span>text</span>}
+      <input type="text" onChange={onChange} />
       <Playstation />
       <Eggs />
       <Ground />
     </>
   );
 };
+
