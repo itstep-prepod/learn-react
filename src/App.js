9050ccc125471c7719c98eafbd3527df654c9def
Web <web@it.step>
2024-06-02 16:38:57 +0300

    main branch default view

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
-import { Playstation } from "./Playstation";
-import { Eggs } from "./Eggs";
-import { Ground } from "./Ground";
+import React from "react";
 
-// TODO input value two way binding
+
 
 export const App = () => {
-  const [shouldShowText, setShouldShowText] = useState(false);
-
-  const onChange = (e) => {
-    console.log(e.target.value);
-  };
-
-  const show = () => {
-    setShouldShowText((prevShowText) => !prevShowText);
-  };
-
-  return (
-    <>
-      <button onClick={show}>show</button>
-      {shouldShowText && <span>text</span>}
-      <input type="text" onChange={onChange} />
-      <Playstation />
-      <Eggs />
-      <Ground />
-    </>
-  );
-};
-
+    return <div>hello world!</div>
+}
