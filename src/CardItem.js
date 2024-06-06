65bc0e4c50b3df398c5018c51d96976e75d0fd1d
Web <web@it.step>
2024-06-06 21:21:39 +0300

    delete persons

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-// dum
 
 export const CardItem = ({name, age, phone}) => {
     return (
         <div>
+            <hr/>
             <h2>{name}</h2>
             <p>{age}</p>
             <p>{phone}</p>
+            <button onClick={() => {}}>delete</button>
+            <hr/>
         </div>
     )
 };
