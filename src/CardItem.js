e91fbe46254e735003654e17c5fb00fa7f436139
Alesia Davydova <143227749+alesiaa111@users.noreply.github.com>
2024-06-09 15:21:03 +0300

    Update CardItem.js
    HomeWork

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-// dum
 
-export const CardItem = ({name, age, phone}) => {
+
+
+
+export const CardItem = ({name, age, phone, onDeletePerson}) => {
     return (
         <div>
-
             <hr/>
             <h2>{name}</h2>
             <p>{age}</p>
             <p>{phone}</p>
-            <button onClick={() => {}}>delete</button>
+            <button onClick={() => {onDeletePerson()}}>delete</button>
             <hr/>
         </div>
     )
